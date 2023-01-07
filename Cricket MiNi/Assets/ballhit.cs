1a373556afc693c9f114ae9c3b0111b351fd67c3
IAmNotGameDev <ms.sazzad.0000@gmail.com>
2023-01-07 17:24:55 +0600

    Basic Score System Added

--- Cricket MiNi/Assets/ballhit.cs
+++ Cricket MiNi/Assets/ballhit.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class ballhit : MonoBehaviour
 {
     public Rigidbody rb;
+    private int ballTounchCount = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("DestroyBall", 5f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void DestroyBall()
+    {
+        Destroy(this.gameObject);
     }
     public void OnTriggerEnter(Collider other)
     {
@@ -27,5 +32,30 @@ public class ballhit : MonoBehaviour
             Vector3 BallDirection = new Vector3(-ballForce, ballHeight, ballPosition);
             rb.AddForce(BallDirection, ForceMode.Impulse);
         }
+        if (other.gameObject.CompareTag("Boundary"))
+        {
+            if (ballTounchCount <= 1)
+            {
+                GameManager.instance.UpdateRuns(6);
+            }
+            else
+            {
+                GameManager.instance.UpdateRuns(4);
+            }
+        }
+
+        if (other.gameObject.CompareTag("Out"))
+        {
+            GameManager.instance.totalRuns = 0;
+            GameManager.instance.UpdateRuns(0);
+        }
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            ballTounchCount++;
+        }
     }
 }
+
