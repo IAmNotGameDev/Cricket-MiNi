a090941b1e65d14aa7ec95f0eadd46af20569e38
IAmNotGameDev <ms.sazzad.0000@gmail.com>
2023-01-20 18:23:30 +0600

    Public Ball Hit Force,Position,Height

--- Cricket MiNi/Assets/ballhit.cs
+++ Cricket MiNi/Assets/ballhit.cs
@@ -9,9 +9,51 @@ public class ballhit : MonoBehaviour
     private int ballTounchCount = 0;
     BatsmanControl bt;
 
+
+    public float LeftForceMin = 0;
+    public float LeftForceMax = 0;
+    public float LeftHeightMin = 0;
+    public float LeftHeightMax = 0;
+    public float LeftPositionMin = 0;
+    public float LeftPositionMax = 0;
+
+
+
+
+
+    public float RightForceMin = 0;
+    public float RightForceMax = 0;
+    public float RightHeightMin = 0;
+    public float RightHeightMax = 0;
+    public float RightPositionMin = 0;
+    public float RightPositionMax = 0;
+
+
+
+
+    public float FrontForceMin = 0;
+    public float FrontForceMax = 0;
+    public float FrontHeightMin = 0;
+    public float FrontHeightMax = 0;
+    public float FrontPositionMin = 0;
+    public float FrontPositionMax = 0;
+
+
+
+
+    public float BackForceMin = 0;
+    public float BackForceMax = 0;
+    public float BackHeightMin = 0;
+    public float BackHeightMax = 0;
+    public float BackPositionMin = 0;
+    public float BackPositionMax = 0;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+
         Invoke("DestroyBall", 5f);
         bt = GameObject.FindObjectOfType<BatsmanControl>();
         Physics.gravity = new Vector3(0, -0.0F, 0);
@@ -20,7 +62,7 @@ public class ballhit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(transform.position);
     }
     public void DestroyBall()
     {
@@ -32,38 +74,44 @@ public class ballhit : MonoBehaviour
         if (other.gameObject.CompareTag("Bat"))
         {
             if (bt.sl == true) {
-            float ballForce = Random.Range(1f, 10f);
-            float ballHeight = Random.Range(1f, 15f);
-            float ballPosition = Random.Range(-5f, 5f);
 
-            Vector3 BallDirection = new Vector3(-ballForce, ballHeight, ballPosition);
+                float ballForceL = Random.Range(LeftForceMin, LeftForceMax);
+                float ballHeightL = Random.Range(LeftHeightMin, LeftHeightMax);
+                float ballPositionL = Random.Range(LeftPositionMin, LeftPositionMax);
+                Vector3 BallDirection = new Vector3(-ballForceL, ballHeightL, ballPositionL);
             rb.AddForce(BallDirection, ForceMode.Impulse);
             }
             if (bt.sf == true)
             {
-                float ballForce = Random.Range(1f, 3f);
-                float ballHeight = Random.Range(1f, 15f);
-                float ballPosition = Random.Range(-5f, 5f);
 
-                Vector3 BallDirection = new Vector3(-ballForce, ballHeight, ballPosition);
+
+                float ballForceF = Random.Range(FrontForceMin, FrontForceMax);
+                float ballHeightF = Random.Range(FrontHeightMin, FrontHeightMax);
+                float ballPositionF = Random.Range(FrontPositionMin, FrontPositionMax);
+
+                Vector3 BallDirection = new Vector3(-ballForceF, ballHeightF, ballPositionF);
                 rb.AddForce(BallDirection, ForceMode.Impulse);
             }
             if (bt.sr == true)
             {
-                float ballForce = Random.Range(-5f, -15f);
-                float ballHeight = Random.Range(1f, 15f);
-                float ballPosition = Random.Range(-5f, 5f);
 
-                Vector3 BallDirection = new Vector3(-ballForce, ballHeight, ballPosition);
+
+                float ballForceR = Random.Range(RightForceMin, RightForceMax);
+                float ballHeightR = Random.Range(RightHeightMin, RightHeightMax);
+                float ballPositionR = Random.Range(RightPositionMin, RightPositionMax);
+
+                Vector3 BallDirection = new Vector3(-ballForceR, ballHeightR, ballPositionR);
                 rb.AddForce(BallDirection, ForceMode.Impulse);
             }
             if (bt.sb == true)
             {
-                float ballForce = Random.Range(-1f, -3f);
-                float ballHeight = Random.Range(1f, 15f);
-                float ballPosition = Random.Range(-5f, 5f);
 
-                Vector3 BallDirection = new Vector3(-ballForce, ballHeight, ballPosition);
+
+                float ballForceB = Random.Range(BackForceMin, BackForceMax);
+                float ballHeightB = Random.Range(BackHeightMin, BackHeightMax);
+                float ballPositionB = Random.Range(BackPositionMin, BackPositionMax);
+
+                Vector3 BallDirection = new Vector3(-ballForceB, ballHeightB, ballPositionB);
                 rb.AddForce(BallDirection, ForceMode.Impulse);
             }
         }
